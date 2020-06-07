2b435c64c0b358476df0e7982a6178316f7d3240
Toni Kaplan <tonarie.k@gmail.com>
2020-06-06 22:54:44 -0400

    Initial Commit

---
+++ musicMeta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  musicMeta
+//
+//  Created by Toni Kaplan on 6/6/20.
+//  Copyright © 2020 Toni Kaplan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
